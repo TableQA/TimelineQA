3e7f6cdaa09902e74fb6f184992083583fddacf2
Jaejin Kim <jaejin.kim.0807@gmail.com>
2023-11-10 13:04:08 +0900

    add medium, dense too

--- scripts/multihop.sh
+++ scripts/multihop.sh
@@ -2,5 +2,19 @@ for i in {0..999}
 do
     python multihopQA/multihopQA.py \
         -q multihopQA/queryfile.csv \
-        -d "src/benchmark/sparse-0"
+        -d "src/benchmark/sparse-$i"
+done
+
+for i in {0..999}
+do
+    python multihopQA/multihopQA.py \
+        -q multihopQA/queryfile.csv \
+        -d "src/benchmark/medium-$i"
+done
+
+for i in {0..999}
+do
+    python multihopQA/multihopQA.py \
+        -q multihopQA/queryfile.csv \
+        -d "src/benchmark/dense-$i"
 done
