6fd98fb8aec6636d5cb402c68b4728fd1cd79a36
baek26 <bje1126@postech.ac.kr>
2023-12-21 16:10:21 +0900

    fix hierarchy execute scripts in the TimelineQA directory

--- scripts/generateDenseDBs.sh
+++ scripts/generateDenseDBs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
 #
@@ -5,12 +6,11 @@
 # LICENSE file in the root directory of this source tree.
 
 
-#!/usr/bin/zsh
-for i in {0..999}
+for i in {0..22}
 do
     echo "Generating dense-$i.json year=2022, seed=$((12345+$i))..."
     \rm -rf benchmark/dense-$i
     mkdir benchmark/dense-$i
-    python3 generateDB.py -y 2022 -s $((12345+$i)) -c "dense" -d benchmark/dense-$i -o dense-$i.json
+    python3 src/generateDB.py -y 2022 -s $((12345+$i)) -c "dense" -d benchmark/dense-$i -o dense-$i.json
     echo "Done!"
 done
