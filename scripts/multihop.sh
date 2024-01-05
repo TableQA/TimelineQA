e7a3f3f246b961693119b852d54a9d2aba9ddd81
Jaejin Kim <jaejin.kim@snu.ac.kr>
2024-01-05 04:52:06 +0000

    change indices

--- scripts/multihop.sh
+++ scripts/multihop.sh
@@ -1,18 +1,18 @@
-for i in {0..999}
+for i in {0..40}
 do
     python multihopQA/multihopQA.py \
         -q multihopQA/queryfile.csv \
         -d "src/benchmark/sparse-$i"
 done
 
-for i in {0..999}
+for i in {0..40}
 do
     python multihopQA/multihopQA.py \
         -q multihopQA/queryfile.csv \
         -d "src/benchmark/medium-$i"
 done
 
-for i in {0..999}
+for i in {0..40}
 do
     python multihopQA/multihopQA.py \
         -q multihopQA/queryfile.csv \
